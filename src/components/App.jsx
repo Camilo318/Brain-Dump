103fd46baf05e1db64baccddc20e261c978b9cc0
Camilo318 <camiloa0318@gmail.com>
2020-12-18 17:54:18 -0500

    Basic set up done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,6 @@ const App = () => {
     return (
         <div>
             <h1>Billie Eilish</h1>
-            <ul>
-                {list.map(loader => (
-                    <li key={loader.id}>{loader.name}</li>
-                ))}
-            </ul>
-
-            <button onClick={handleClick}>Show Loaders</button>
         </div>
     )
 }
