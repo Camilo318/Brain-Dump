aeb8f00ca3585c50ba4b8e8e1eb65e465b1ca4fb
Camilo318 <camiloa0318@gmail.com>
2020-12-18 21:56:07 -0500

    Add input to add todo

---
+++ src/components/Add.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Add = () => {
+    return (
+        <div className='add'>
+            <input type="text" placeholder='Add your stuff'/>
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default Add
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'   
 import Navigation from './Navigation'
 import Container from './Container'
+import Add from './Add'
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Header />
             <Container >
                 <Navigation />
+                <Add />
             </Container>
         </>
     )
