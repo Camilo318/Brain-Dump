21ba332407b72de55b3d2c90680869a89794eb1f
Camilo318 <camiloa0318@gmail.com>
2020-12-22 17:38:51 -0500

    Add useRef to keep track of the clicked item and apply the correct style

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useRef }from 'react'
 import { useDispatch } from 'react-redux'
 
 const Navigation = () => {
     const dispatch = useDispatch()
+    const currentView = useRef()
 
     const hadleClick = e => {
         const newView = e.target.dataset.view
+        currentView.current?.classList.remove('current')
+        currentView.current = e.target
+        currentView.current.classList.add('current')
         if (!newView) return 
         dispatch({
             type: 'views/update-view',
