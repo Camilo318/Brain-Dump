3e7bd14d0669bf2d2f8bb1af886f84a3e59e1f06
Camilo318 <camiloa0318@gmail.com>
2020-12-22 11:17:22 -0500

    Add toggle action in TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 const TodoItem = ({ task }) => {
+    const dispatch = useDispatch()
+    const checked = task.completed
+    const toggleTask = () => {
+        dispatch({
+            type: 'tasks/toggle-task',
+            payload: task.id
+        })
+    }
+
     return (
         <div className='todo-item'>
-            <input type="checkbox"/>
-            { task.text }
+            <input type='checkbox'
+            onClick={toggleTask}
+            defaultChecked={checked}/>
+            <span>{ task.text }</span>
         </div>
     )
 }
