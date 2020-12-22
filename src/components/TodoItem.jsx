d79334082093fa27612d1a30d669ff3492bb5f93
Camilo318 <camiloa0318@gmail.com>
2020-12-21 19:36:43 -0500

    Add styles to TodoList and TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const TodoItem = ({ task }) => {
     return (
         <div className='todo-item'>
+            <input type="checkbox"/>
             { task.text }
         </div>
     )
