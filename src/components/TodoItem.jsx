c4c91776a58ec1fdea7b907a97b97d00aaefaa6b
Camilo318 <camiloa0318@gmail.com>
2020-12-23 11:44:29 -0500

    Add new icons

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { toggleTask, deleteTask } from '../features/tasks/actions'
-import del from '../assets/images/remove.svg'
+import del from '../assets/images/trash.svg'
+import edit from '../assets/images/edit.svg'
 
 const TodoItem = ({ task }) => {
     const dispatch = useDispatch()
@@ -19,7 +20,14 @@ const TodoItem = ({ task }) => {
             <input type='checkbox'
             onClick={toggle}
             defaultChecked={checked}/>
+
             <span>{ task.text }</span>
+
+            <img
+            className='edit'
+            src={edit}
+            alt={'Edit task'}/>
+            
             <img
             onClick={remove}
             src={del}
